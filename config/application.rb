ab0af736d8135086cc1cd549198166d4ceed8df0
Roberto Salazar <roberto.salazar@sbrforum.com>
2018-12-15 23:56:33 -0600

    Add header configuration

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,13 @@ module Urlshortapiruby
       'Access-Control-Allow-Origin' => '*',
       'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
     }    
-
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+          origins '*' #-> has to be "*" or specific
+          resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
