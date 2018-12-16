a6657159931a0f7932dae4f4aa46d0a9dd97e40d
Roberto Salazar <roberto.salazar@sbrforum.com>
2018-12-16 00:21:51 -0600

    Add changes header access

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'rack/cors'
 
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,13 +14,12 @@ module Urlshortapiruby
       'Access-Control-Allow-Origin' => '*',
       'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
     }    
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
-          origins '*' #-> has to be "*" or specific
-          resource '*', headers: :any, methods: [:get, :post, :options]
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
